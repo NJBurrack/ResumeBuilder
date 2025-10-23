60512127806e57558269cedffe1e92e8017dbae0
NaimStuDCI <naim.burrack@dci-student.org>
2025-10-23 12:02:59 +0200

    Update settings.py with env loading and finalize backend configs

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -137,8 +137,20 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+
+
+
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
+
+
+
+
 
 
 # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
