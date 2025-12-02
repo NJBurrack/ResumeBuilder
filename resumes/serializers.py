49ffa134dcbc9af69a6d29fce8592e141c5d5a4c
NaimStuDCI <naim.burrack@dci-student.org>
2025-12-02 20:21:46 +0100

    feat(resumes): support nested resume updates

--- resumes/serializers.py
+++ resumes/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Resume, Skill, JobHistory, EducationHistory
 
 
-
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
@@ -19,7 +18,6 @@ class EducationHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = EducationHistory
         fields = ['id', 'institution', 'degree', 'start_date', 'end_date']
-
 
 
 class ResumeSerializer(serializers.ModelSerializer):
@@ -40,12 +38,9 @@ class ResumeSerializer(serializers.ModelSerializer):
             'job_history_order',
             'skills_order',
             'education_history_order',
-
         ]
 
     def create(self, validated_data):
-
-
         job_history_data = validated_data.pop('job_history', [])
         skills_data = validated_data.pop('skills', [])
         education_data = validated_data.pop('education_history', [])
@@ -62,30 +57,30 @@ class ResumeSerializer(serializers.ModelSerializer):
         return resume
 
     def update(self, instance, validated_data):
-        
+
         job_history_data = validated_data.pop('job_history', None)
+
         skills_data = validated_data.pop('skills', None)
+        
         education_data = validated_data.pop('education_history', None)
-        
-        
+
         for field, value in validated_data.items():
             setattr(instance, field, value)
         instance.save()
-        
-        
-        if job_history_data is not None:
-            instance.job_history.all().delete()
-            for job in job_history_data:
-                JobHistory.objects.create(resume=instance, **job)
-        
+
         if skills_data is not None:
             instance.skills.all().delete()
             for skill in skills_data:
                 Skill.objects.create(resume=instance, **skill)
-        
+
+        if job_history_data is not None:
+            instance.job_history.all().delete()
+            for job in job_history_data:
+                JobHistory.objects.create(resume=instance, **job)
+
         if education_data is not None:
             instance.education_history.all().delete()
             for edu in education_data:
                 EducationHistory.objects.create(resume=instance, **edu)
-        
+
         return instance
